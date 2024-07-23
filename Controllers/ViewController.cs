f1d0b3e92da9f779bb48791f21df4cb31d8e902d
Paul Menefee <pmenefee@wbf.com>
2024-07-23 08:01:29 -0400

    Corrected grid alignment

---
+++ Controllers/ViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthenticationServer.Controllers
+{
+    public class ViewController : Controller
+    {
+        // GET: View
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
